73b93ef4f47146e1a2e5ec38fb5011c10074d883
krbu06001 <krbu06001@E-PW05CGQY>
2024-11-09 11:26:01 +0100

    Add project files.

---
+++ Inlamningsuppgift_3.5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System; 
+
+namespace Inlamning_3._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            int tal1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Skriv ner en till tal");
+            int tal2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Välj ett räknesätt. (Sriv nummern till räknesättet");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtration");
+            Console.WriteLine("3. Multiplikation");
+            Console.WriteLine("4. Divition");
+            int sätt = int.Parse(Console.ReadLine());
+            int resultat = tal1 - tal2;
+
+            switch (sätt)
+            {
+                case 1:
+                    Console.WriteLine("Svaret är " + tal1 + tal2);
+                    break;
+                case 2:  
+                    Console.WriteLine("Svaret är " + resultat);
+                    break;
+                case 3:
+                    Console.WriteLine("Svaret är " + tal1 * tal2);
+                    break;
+                case 4:
+                    Console.WriteLine("Svaret är " + tal1 / tal2);
+                    break;
+            }
+        }
+    }
+}
